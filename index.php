733e2615274ba68f1991b6442f8e73570773f0c8
Dhananjay P Shinkar <dps48@afsconnect1.njit.edu>
2017-10-01 20:44:02 -0400

    Compare $date with $tar

--- index.php
+++ index.php
@@ -7,6 +7,23 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
+
+echo '<hr>';
+echo '<h1>Compare $date with $tar </h1>';
+
+if($date == $tar)
+	{
+    	echo 'Opps';
+	} else if ($date > $tar)
+		{
+    		echo 'Future';
+		}
+			else if ($date < $tar)
+				{
+					echo 'Past';
+				}
+echo '<hr>';
+
 
 ?>
